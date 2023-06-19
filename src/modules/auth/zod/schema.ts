f15fa723194930b898844070fc31189ab1ab8065
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2023-06-19 22:21:31 +0300

    Update validation schema

--- src/modules/auth/zod/schema.ts
+++ src/modules/auth/zod/schema.ts
@@ -3,17 +3,21 @@ import { object, string, TypeOf } from 'zod';
 export const schema = object({
   firstName: string()
     .nonempty('First name is required')
-    .max(25, 'First name must be less than 25 characters')
+    .max(15, 'First name must be less than 15 characters')
     .optional(),
   lastName: string()
     .nonempty('Last name is required')
-    .max(25, 'Last name must be less than 25 characters')
+    .max(15, 'Last name must be less than 15 characters')
     .optional(),
   email: string().nonempty('Email is required').email('Email is invalid'),
   password: string()
     .nonempty('Password is required')
-    .min(6, 'Password must be more than 8 characters')
-    .max(20, 'Password must be less than 32 characters'),
+    .min(6, 'Password must be more than 6 characters')
+    .max(20, 'Password must be less than 20 characters')
+    .regex(/^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$/, {
+      message:
+        'Password must contain at least 1 uppercase letter and 1 number, and no special characters',
+    }),
 });
 
 export type FormInputs = TypeOf<typeof schema>;
